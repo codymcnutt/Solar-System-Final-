bbeb59a2e21edf5a4e61d40bc1cc848b649ac969
Erich Robinson <robinson.erich@gmail.com>
2015-10-20 15:55:44 -0600

    needs directional light source, sky, orbit planes

---
+++ app.js
@@ -0,0 +1,23 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// Create Express App Object \\
+var app = express();
+
+// Application Configuration \\
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/'));
+
+// Routes \\
+app.get('/', function(req, res){
+  res.send("index.html")
+});
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
